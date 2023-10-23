b4a136d397b5fd7951dc59dde16239ba0bf0869f
Cezar-Ferreira <113953683+Cezar-Ferreira@users.noreply.github.com>
2023-10-23 11:16:47 -0300

    Exercicio modulo 8

---
+++ src/Exercicio.java
@@ -0,0 +1,15 @@
+public class Exercicio {
+
+    public static void main(String args[]) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 40;
+        int num5 = 4;
+
+        int num6 = (num1 + num2 + num3 + num4)/num5;
+
+        System.out.println(num6);
+
+    }
+}
